dc0bf9810934ede72d77764e450bd938f7eac8fb
Roamin <roamin@tuta.io>
2023-10-02 21:20:28 +0000

    remove swagger page

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Transpile Swagger UI React https://github.com/swagger-api/swagger-ui/issues/8245
-  transpilePackages: [
-    "react-syntax-highlighter",
-    "swagger-client",
-    "swagger-ui-react",
-  ],
-}
+const nextConfig = {}
 
 module.exports = nextConfig
