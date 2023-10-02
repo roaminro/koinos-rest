835ba1aa35d8d21e54cc45631291c0221eec740a
Roamin <roamin@tuta.io>
2023-10-02 21:05:17 +0000

    fix swagger

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  // Transpile Swagger UI React https://github.com/swagger-api/swagger-ui/issues/8245
+  transpilePackages: [
+    "react-syntax-highlighter",
+    "swagger-client",
+    "swagger-ui-react",
+  ],
+}
 
 module.exports = nextConfig
