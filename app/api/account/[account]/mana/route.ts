f298aedda7dc2f443277ac14c38ceda1872cf953
Greg Thomas Andes <andesgregthomas@gmail.com>
2023-11-01 22:39:24 +0800

    in progress: history endpoint
    in progress: history records getter function

--- app/api/account/[account]/mana/route.ts
+++ app/api/account/[account]/mana/route.ts
@@ -1,7 +1,7 @@
-import { getAddress } from '@/utils/addresses'
-import { AppError, getErrorMessage, handleError } from '@/utils/errors'
-import { getProvider } from '@/utils/providers'
-import { utils } from 'koilib'
+import { getAddress } from "@/utils/addresses";
+import { AppError, getErrorMessage, handleError } from "@/utils/errors";
+import { getProvider } from "@/utils/providers";
+import { utils } from "koilib";
 
 /**
  * @swagger
@@ -16,7 +16,9 @@ import { utils } from 'koilib'
  *        in: path
  *        description: Koinos address of the account, name of the account (for system contracts) or KAP name
  *        required: true
+ *
  *      - $ref: '#/components/parameters/X-JSON-RPC-URL'
+ *
  *     responses:
  *       200:
  *        description: Value
@@ -25,19 +27,22 @@ import { utils } from 'koilib'
  *            schema:
  *              $ref: '#/components/schemas/Value'
  */
-export async function GET(request: Request, { params }: { params: { account: string } }) {
+export async function GET(
+  request: Request,
+  { params }: { params: { account: string } }
+) {
   try {
-    const provider = getProvider()
-    const account = await getAddress(params.account)
+    const provider = getProvider();
+    const account = await getAddress(params.account);
     try {
-      const rc = await provider.getAccountRc(account)
+      const rc = await provider.getAccountRc(account);
       return Response.json({
-        value: utils.formatUnits(rc, 8)
-      })
+        value: utils.formatUnits(rc, 8),
+      });
     } catch (error) {
-      throw new AppError(getErrorMessage(error as Error))
+      throw new AppError(getErrorMessage(error as Error));
     }
   } catch (error) {
-    return handleError(error as Error)
+    return handleError(error as Error);
   }
 }
