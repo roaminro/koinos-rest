e33c17b684c162aac56b9165d16434f7e49da919
Greg Thomas Andes <andesgregthomas@gmail.com>
2023-11-03 10:37:01 +0800

    pruned other issues unrelated to decode operations

--- app/swagger/page.tsx
+++ app/swagger/page.tsx
@@ -1,18 +1,18 @@
-import { createSwaggerSpec } from "next-swagger-doc";
-import ReactSwagger from "./react-swagger";
-import "swagger-ui-react/swagger-ui.css";
-import SwaggerSpecs from "@/swagger.specs.json";
+import { createSwaggerSpec } from "next-swagger-doc"
+import ReactSwagger from "./react-swagger"
+import "swagger-ui-react/swagger-ui.css"
+import SwaggerSpecs from "@/swagger.specs.json"
 
 const getApiDocs = async () => {
-  const spec = createSwaggerSpec(SwaggerSpecs);
-  return spec;
-};
+  const spec = createSwaggerSpec(SwaggerSpecs)
+  return spec
+}
 
 export default async function IndexPage() {
-  const spec = await getApiDocs();
+  const spec = await getApiDocs()
   return (
     <section className="container">
       <ReactSwagger spec={spec} />
     </section>
-  );
+  )
 }
