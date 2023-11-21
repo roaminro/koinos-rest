43e7070812582e5b1884284d4ef2b393e472c846
Greg Thomas Andes <andesgregthomas@gmail.com>
2023-11-21 19:42:33 +0800

    enhanced description; added example req/res

--- app/api/decode/events/route.ts
+++ app/api/decode/events/route.ts
@@ -8,7 +8,7 @@ import { decodeEvents } from '@/utils/events'
  * /api/decode/events:
  *   post:
  *     tags: [Decode]
- *     description: This endpoint takes an array of "encoded" events and returns an array of "decoded" events.
+ *     description: This endpoint takes an array of "encoded" events and returns an array of "decoded" events. Feel free to test the example request body and response below before testing out your own data.
  *
  *     parameters:
  *      - $ref: '#/components/parameters/X-JSON-RPC-URL'
@@ -22,6 +22,18 @@ import { decodeEvents } from '@/utils/events'
  *             type: array
  *             items:
  *               $ref: '#/components/schemas/EncodedEvent'
+ *             example:
+ *               - call_contract:
+ *                   source: '18tWNU7E4yuQzz7hMVpceb9ixmaWLVyQsr'
+ *                   name: 'koinos.contracts.token.burn_event'
+ *                   data: 'ChkA7-Mh3yERswBXFp2UPvegxIiGAauR1O_zEM2RkHE='
+ *                   impacted: ["1NsQbH5AhQXgtSNg1ejpFqTi2hmCWz1eQS"]
+ *               - call_contract:
+ *                   sequence: 1
+ *                   source: '15DJN4a8SgrbGhhGksSBASiSYjGnMU8dGL'
+ *                   name: 'koinos.contracts.token.mint_event'
+ *                   data: 'ChkA7-Mh3yERswBXFp2UPvegxIiGAauR1O_zELW7zHU='
+ *                   impacted: ["1NsQbH5AhQXgtSNg1ejpFqTi2hmCWz1eQS"]
  *
  *     responses:
  *       200:
@@ -29,7 +41,23 @@ import { decodeEvents } from '@/utils/events'
  *         content:
  *           application/json:
  *             schema:
- *               type: object
+ *               type: array
+ *               example:
+ *                 - call_contract:
+ *                    source: '18tWNU7E4yuQzz7hMVpceb9ixmaWLVyQsr'
+ *                    name: 'koinos.contracts.token.burn_event'
+ *                    data:
+ *                      from: "1NsQbH5AhQXgtSNg1ejpFqTi2hmCWz1eQS"
+ *                      value: "237242573"
+ *                    impacted: ["1NsQbH5AhQXgtSNg1ejpFqTi2hmCWz1eQS"]
+ *                 - call_contract:
+ *                    sequence: 1
+ *                    source: '15DJN4a8SgrbGhhGksSBASiSYjGnMU8dGL'
+ *                    name: 'koinos.contracts.token.mint_event'
+ *                    data:
+ *                      to: "1NsQbH5AhQXgtSNg1ejpFqTi2hmCWz1eQS"
+ *                      value: "246619573"
+ *                    impacted: ["1NsQbH5AhQXgtSNg1ejpFqTi2hmCWz1eQS"]
  *
  * components:
  *   schemas:
