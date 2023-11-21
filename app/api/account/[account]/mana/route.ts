f07e4705c9eb171ad3d1670210bd6e7a45429aba
Greg Thomas Andes <andesgregthomas@gmail.com>
2023-11-21 20:39:44 +0800

    Update route.ts
    enhanced description; added placeholder input & example response

--- app/api/account/[account]/mana/route.ts
+++ app/api/account/[account]/mana/route.ts
@@ -8,36 +8,38 @@ import { utils } from 'koilib'
  * /api/account/{account}/mana:
  *   get:
  *     tags: [Accounts]
- *     description: Returns the account's mana
+ *     description: Return the account's available mana.
  *     parameters:
  *      - name: account
+ *        in: path
  *        schema:
  *          type: string
- *        in: path
+ *          example: 1NsQbH5AhQXgtSNg1ejpFqTi2hmCWz1eQS
  *        description: Koinos address of the account, name of the account (for system contracts) or KAP name
  *        required: true
- *
  *      - $ref: '#/components/parameters/X-JSON-RPC-URL'
- *
  *     responses:
  *       200:
  *        description: Value
  *        content:
  *          application/json:
  *            schema:
- *              $ref: '#/components/schemas/Value'
+ *              type: object
+ *              properties:
+ *                value:
+ *                  type: string
+ *            example:
+ *              value: "19.43467354"
  */
-export async function GET(
-  request: Request,
-  { params }: { params: { account: string } }
-) {
+
+export async function GET(request: Request, { params }: { params: { account: string } }) {
   try {
     const provider = getProvider()
     const account = await getAddress(params.account)
     try {
       const rc = await provider.getAccountRc(account)
       return Response.json({
-        value: utils.formatUnits(rc, 8),
+        value: utils.formatUnits(rc, 8)
       })
     } catch (error) {
       throw new AppError(getErrorMessage(error as Error))
