bfc9e28a40d240049177b13c3a80e9a0fe6d2996
Roamin <97863888+roaminro@users.noreply.github.com>
2023-11-25 23:21:55 +0000

    cors

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,30 @@
 import { NextResponse } from 'next/server'
 
 // CORS headers for the api endpoints
-const headers = {
+const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization'
 }
 
 export function middleware(request: Request) {
+  const response = NextResponse.next()
+
   if (request.method === 'OPTIONS') {
-    return NextResponse.json(null, { status: 204, headers })
+    return NextResponse.json(null, { status: 204, headers: corsHeaders })
   }
+
+  response.headers.append('Access-Control-Allow-Origin', corsHeaders['Access-Control-Allow-Origin'])
+  response.headers.append(
+    'Access-Control-Allow-Methods',
+    corsHeaders['Access-Control-Allow-Methods']
+  )
+  response.headers.append(
+    'Access-Control-Allow-Headers',
+    corsHeaders['Access-Control-Allow-Headers']
+  )
+
+  return response
 }
 
 export const config = {
