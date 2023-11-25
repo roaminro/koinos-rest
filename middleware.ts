39625df20f23e50801dbcf54fb958b380eebf7b0
Roamin <97863888+roaminro@users.noreply.github.com>
2023-11-25 22:56:29 +0000

    add CORS

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+
+// CORS headers for the api endpoints
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+}
+
+export function middleware(request: Request) {
+  if (request.method === 'OPTIONS') {
+    return NextResponse.json({}, { headers })
+  }
+}
+
+export const config = {
+  matcher: '/api/:path*'
+}
