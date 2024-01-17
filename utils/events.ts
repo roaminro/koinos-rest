ad061c9d44efc190df227552f0116b3995e23099
Roamin <97863888+roaminro@users.noreply.github.com>
2024-01-17 01:21:34 +0000

    feature: improve events decoding for non-compliant tokens

--- utils/events.ts
+++ utils/events.ts
@@ -22,17 +22,20 @@ export async function decodeEvents(events: interfaces.EventData[]) {
       if (contract && contract.functions) {
         try {
           // patch event name for non-compliant token contracts
-          if (event.name.endsWith('token.burn_event') || event.name.endsWith('token.burn')) {
+          if (event.name.endsWith('token.burn_event')) {
             event.name = 'koinos.contracts.token.burn_event'
-          } else if (event.name.endsWith('token.mint_event') || event.name.endsWith('token.mint')) {
+          } else if (event.name.endsWith('token.mint_event')) {
             event.name = 'koinos.contracts.token.mint_event'
-          } else if (
-            event.name.endsWith('token.transfer_event') ||
-            event.name.endsWith('token.transfer')
-          ) {
+          } else if (event.name.endsWith('token.transfer_event')) {
             event.name = 'koinos.contracts.token.transfer_event'
           } else if (event.name.endsWith('token.approve')) {
             event.name = 'token.approve_event'
+          } else if (event.name.endsWith('token.burn')) {
+            event.name = 'token.burn_event'
+          } else if (event.name.endsWith('token.mint')) {
+            event.name = 'token.mint_event'
+          } else if (event.name.endsWith('token.transfer')) {
+            event.name = 'token.transfer_event'
           }
 
           const decodedEvent = await contract.decodeEvent(event)
